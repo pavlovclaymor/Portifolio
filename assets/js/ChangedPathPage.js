1b6564412c1ea78ad5037bc362e1d46d0af57d47
Ronaldo Caminha Bernardo <pavlov2006claymor@gmail.com>
2024-04-20 23:36:55 +0200

    Portifolio finalizado devido a falta de tempo mais vou melhorar eu juro

---
+++ assets/js/ChangedPathPage.js
@@ -0,0 +1,12 @@
+function Changed(){
+    window.location.href = "../page/AboutMePage.html"
+}
+function HomePage(){
+    window.location.href = "../page/index.html"
+}
+function HomePageUpDate(){
+    window.location.reload = "../page/index.html"
+}
+function BtnMyContact(){
+
+}
